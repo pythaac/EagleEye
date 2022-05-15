bfb406debcc97416cfabcfdaa7b8ad6973500bb9
pythaac <pythaac@gmail.com>
2022-05-15 15:27:09 +0900

    [EagleEye] Controller added

---
+++ test/slow-service/src/main/java/com/example/slowservice/SlowController.java
@@ -0,0 +1,21 @@
+package com.example.slowservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SlowController {
+    private final static Long target = 10_000_000_000L;
+
+    @GetMapping(value = "/slow")
+    public Long normal(){
+        Long rand = (long)(Math.random() * 1000) - 500;
+
+        long result = 0L;
+        for (long t=0; t<rand+target; t++){
+            result += t;
+        }
+
+        return result;
+    }
+}
